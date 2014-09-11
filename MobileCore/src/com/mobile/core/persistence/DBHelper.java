173943ad0c5061d4c7e96ce95736f1ad8f7f7e80
yj <yj5858@gmail.com>
2014-09-11 10:03:07 +0800

    update

--- MobileCore/src/com/mobile/core/persistence/DBHelper.java
+++ MobileCore/src/com/mobile/core/persistence/DBHelper.java
@@ -1,30 +1,141 @@
 package com.mobile.core.persistence;
 
 
+
+
 import com.google.inject.Inject;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.provider.BaseColumns;
 
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
     private static final String NAME = "mobile.db";
+	private SQLiteDatabase db;
     /**
      * @param context
      */
-    @Inject
+   @Inject
 	public DBHelper(Context context) {
 		super(context, NAME, null, VERSION);
 	}
-
+//	private static DBHelper mdbHelper;
+//	
+//	public static DBHelper getInstance(Context context)
+//	{
+//		if(mdbHelper==null)
+//		{
+//			mdbHelper=new DBHelper(context);
+//		}
+//		return mdbHelper;
+//	}
+	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
+		this.db = db;
+		operateTable(db, "");
 	}
-
+	private DBHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		if (oldVersion == newVersion) {
+			return;
+		}
+		operateTable(db, "DROP TABLE IF EXISTS ");
+		onCreate(db);
 	}
 
+	public void operateTable(SQLiteDatabase db, String actionString) {
+		Class<DatabaseColumn>[] columnsClasses = DatabaseColumn.getSubClasses();
+		DatabaseColumn columns = null;
+
+		for (int i = 0; i < columnsClasses.length; i++) {
+			try {
+				columns = columnsClasses[i].newInstance();
+				if ("".equals(actionString) || actionString == null) {
+					db.execSQL(columns.getTableCreateor());
+				} else {
+					db.execSQL(actionString + columns.getTableName());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public long insert(String Table_Name, ContentValues values) {
+		if (db == null)
+			db = getWritableDatabase();
+		return db.insert(Table_Name, null, values);
+	}
+
+	/**
+	 * 
+	 * @param Table_Name
+	 * @param id
+	 * @return 影响行数
+	 */
+	public int delete(String Table_Name, int id) {
+		if (db == null)
+			db = getWritableDatabase();
+		return db.delete(Table_Name, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(id) });
+	}
+
+	/**
+	 * @param Table_Name
+	 * @param values
+	 * @param WhereClause
+	 * @param whereArgs
+	 * @return 影响行数
+	 */
+	public int update(String Table_Name, ContentValues values,
+			String WhereClause, String[] whereArgs) {
+		if (db == null) {
+			db = getWritableDatabase();
+		}
+		return db.update(Table_Name, values, WhereClause, whereArgs);
+	}
+
+	public Cursor query(String Table_Name, String[] columns, String whereStr,
+			String[] whereArgs) {
+		if (db == null) {
+			db = getReadableDatabase();
+		}
+		return db.query(Table_Name, columns, whereStr, whereArgs, null, null,
+				null);
+	}
+
+	public Cursor rawQuery(String sql, String[] args) {
+		if (db == null) {
+			db = getReadableDatabase();
+		}
+		return db.rawQuery(sql, args);
+	}
+
+	public void ExecSQL(String sql) {
+		if (db == null) {
+			db = getWritableDatabase();
+		}
+		db.execSQL(sql);
+	}
+
+	public void closeDb() {
+		if (db != null) {
+			db.close();
+			db = null;
+		}
+	}
 }
